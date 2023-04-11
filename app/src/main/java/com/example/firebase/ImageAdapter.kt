204fbdb5dac1e9cc7b595b6e1da965a7046124d7
xieyz10 <54917731+xieyz10@users.noreply.github.com>
2023-04-11 10:43:37 -0400

    add blue tooth

--- app/src/main/java/com/example/firebase/ImageAdapter.kt
+++ app/src/main/java/com/example/firebase/ImageAdapter.kt
@@ -1,9 +1,16 @@
+import android.bluetooth.BluetoothAdapter
+import android.content.ContentUris
 import android.content.Intent
+import android.net.Uri
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
 import android.widget.ImageView
+import android.widget.Toast
+import androidx.core.app.ActivityCompat.startActivityForResult
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.firebase.FullImageActivity
@@ -15,8 +22,9 @@ class ImagesAdapter(
 ) : RecyclerView.Adapter<ImagesAdapter.ImageViewHolder>() {
 
     class ImageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val image: ImageView = itemView.findViewById(R.id.imageView)
-        val deleteButton: ImageButton = itemView.findViewById(R.id.deleteButton)
+        private val image: ImageView = itemView.findViewById(R.id.imageView)
+        private val deleteButton: ImageButton = itemView.findViewById(R.id.deleteButton)
+        private val bluetoothButton: ImageButton = itemView.findViewById(R.id.bluetoothButton)
 
         fun bind(imageUrl: String, onDeleteClickListener: ((Int) -> Unit)?) {
             Glide.with(itemView.context)
@@ -28,13 +36,41 @@ class ImagesAdapter(
                 onDeleteClickListener?.invoke(adapterPosition)
             }
 
-            // Set click listener
+            // Set click listener for full image view
             itemView.setOnClickListener {
                 val intent = Intent(itemView.context, FullImageActivity::class.java).apply {
                     putExtra("image_url", imageUrl)
                 }
                 itemView.context.startActivity(intent)
             }
+
+            // Set click listener for Bluetooth button
+            bluetoothButton.setOnClickListener {
+                val intent = Intent()
+                intent.action = Intent.ACTION_SEND
+                intent.type = "image/*"
+                intent.putExtra(Intent.EXTRA_STREAM, getImageUri(imageUrl))
+                itemView.context.startActivity(Intent.createChooser(intent, "Share image via"))
+            }
+        }
+
+        private fun getImageUri(imageUrl: String): Uri? {
+            val mediaUri: Uri? = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+            val projection = arrayOf(MediaStore.Images.Media._ID)
+            val selection = "${MediaStore.Images.Media.DATA}=?"
+            val selectionArgs = arrayOf(imageUrl)
+            val cursor = itemView.context.contentResolver.query(
+                mediaUri!!,
+                projection,
+                selection,
+                selectionArgs,
+                null
+            )
+            if (cursor != null && cursor.moveToFirst()) {
+                val id = cursor.getLong(cursor.getColumnIndexOrThrow(MediaStore.Images.Media._ID))
+                return ContentUris.withAppendedId(mediaUri, id)
+            }
+            return null
         }
     }
 
